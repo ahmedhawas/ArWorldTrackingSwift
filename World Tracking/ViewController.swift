acf0021145d7a9a26dd5daec63931e34e5e0dfad
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2017-09-24 18:17:03 -0400

    can add cube to world

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -25,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        node.position = SCNVector3(0,0,-0.3)
+        self.sceneView.scene.rootNode.addChildNode(node) // node gets positioned relative to root node
+    }
+    
 }
 
