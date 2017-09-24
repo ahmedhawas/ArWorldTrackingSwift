bdb8463882d7461ed6b2d9ada9547c22c9bbf7c5
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2017-09-24 17:20:01 -0400

    initial commit. Added Arkit to controller

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration() // track devise relative to position
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
