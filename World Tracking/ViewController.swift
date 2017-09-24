6c2f03400c83f1bc10b26ee17623ed779a97b67b
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2017-09-24 17:12:31 -0400

    Initial Commit

---
+++ World Tracking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  World Tracking
+//
+//  Created by Ahmed Hawas on 2017-09-24.
+//  Copyright © 2017 Ahmed Hawas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
